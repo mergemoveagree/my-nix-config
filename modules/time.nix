bd44262a60fb2a3cd3d526051908967c37743e6e
Jaden Nola <hi@jaden.rs>
2025-09-16 22:04:05 -0500

    feat: automatic timezone

--- modules/time.nix
+++ modules/time.nix
@@ -7,30 +7,35 @@ let
     "time.cifelli.xyz"
     "time.txryan.com"
   ];
-in {
-  flake.modules.nixos.base = {
-    services.chrony = {
-      enable = true;
-      enableNTS = true;
-      #enableRTCTrimming = config.hostSpec.hasRtc;
-      servers = timeServers;
-      serverOption = "iburst";
-      #extraFlags = lib.optional (! config.hostSpec.hasRtc) "-s";
-      extraConfig = ''
-        port 0
-        cmdport 0
-        hwtimestamp *
-      '';
-    };
+in
+  {lib, ...}: {
+    flake.modules.nixos.base = {
+      services.automatic-timezoned.enable = true;
+      # https://github.com/NixOS/nixpkgs/issues/68489#issuecomment-1484030107
+      services.geoclue2.enableDemoAgent = lib.mkForce true;
 
-    networking.timeServers = timeServers;
-    networking.hosts = {
-      "3.134.129.152" = ["ohio.time.system76.com"];
-      "52.10.183.132" = ["oregon.time.system76.com"];
-      "3.220.42.39" = ["virginia.time.system76.com"];
-      "143.42.132.139" = ["stratum1.time.cifelli.xyz"];
-      "50.116.42.84" = ["time.cifelli.xyz"];
-      "15.204.249.252" = ["time.txryan.com"];
+      services.chrony = {
+        enable = true;
+        enableNTS = true;
+        #enableRTCTrimming = config.hostSpec.hasRtc;
+        servers = timeServers;
+        serverOption = "iburst";
+        #extraFlags = lib.optional (! config.hostSpec.hasRtc) "-s";
+        extraConfig = ''
+          port 0
+          cmdport 0
+          hwtimestamp *
+        '';
+      };
+
+      networking.timeServers = timeServers;
+      networking.hosts = {
+        "3.134.129.152" = ["ohio.time.system76.com"];
+        "52.10.183.132" = ["oregon.time.system76.com"];
+        "3.220.42.39" = ["virginia.time.system76.com"];
+        "143.42.132.139" = ["stratum1.time.cifelli.xyz"];
+        "50.116.42.84" = ["time.cifelli.xyz"];
+        "15.204.249.252" = ["time.txryan.com"];
+      };
     };
-  };
-}
+  }
