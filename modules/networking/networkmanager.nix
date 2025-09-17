f4e1a298ab80de0929d162a2b72e64fe11c90d91
Jaden Nola <hi@jaden.rs>
2025-09-17 10:30:15 -0500

    feat: add owner to networkmanager group

--- modules/networking/networkmanager.nix
+++ modules/networking/networkmanager.nix
@@ -1,8 +1,10 @@
-{
+{config, ...}: {
   flake.modules.nixos.pc = {
     networking.networkmanager = {
       enable = true;
       wifi.backend = "iwd";
     };
+
+    users.users.${config.flake.meta.owner.username}.extraGroups = ["networkmanager"];
   };
 }
