da7d2597e820f33ec29808ecdabcfb9df633c730
Jaden Nola <hi@jaden.rs>
2025-09-17 10:46:20 -0500

    feat: enable systemd in initrd in base

--- modules/boot/systemd-boot.nix
+++ modules/boot/systemd-boot.nix
@@ -1,6 +1,3 @@
 {
-  flake.modules.nixos.systemd-boot = {
-    boot.loader.systemd-boot.enable = true;
-    boot.initrd.systemd.enable = true;
-  };
+  flake.modules.nixos.systemd-boot.boot.loader.systemd-boot.enable = true;
 }
