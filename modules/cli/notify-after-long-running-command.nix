3e3b08955c3f33b60357611d142ec3e057598517
Jaden Nola <hi@jaden.rs>
2025-09-20 18:04:35 -0500

    feat: ignore some long running notifications

--- modules/cli/notify-after-long-running-command.nix
+++ modules/cli/notify-after-long-running-command.nix
@@ -1,11 +1,20 @@
-{inputs, ...}: {
+{
+  inputs,
+  lib,
+  ...
+}: {
   flake.modules.homeManager.gui = {pkgs, ...}: {
     home.packages = [pkgs.libnotify];
-    programs.zsh.plugins = [
-      {
-        name = "auto-notify";
-        src = inputs.zsh-auto-notify;
-      }
-    ];
+    programs.zsh = {
+      initContent = lib.mkAfter ''
+        AUTO_NOTIFY_IGNORE+=("hx" "yazi" "jj")
+      '';
+      plugins = [
+        {
+          name = "auto-notify";
+          src = inputs.zsh-auto-notify;
+        }
+      ];
+    };
   };
 }
